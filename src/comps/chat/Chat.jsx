31e14cd6dfe36c029ce80e3a3bdbff7c41a2a6f6
BakerMoney <s2027736@ed.ac.uk>
2025-07-26 18:13:26 +0100

    Chat section implemented

--- src/comps/chat/Chat.jsx
+++ src/comps/chat/Chat.jsx
@@ -1,8 +1,131 @@
+import { useState } from "react";
 import "./chat.css"
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
+const [open,setOpen] = useState(false)
+const [text,setText] = useState("")
+
+const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false)
+};
     return(
-        <div className="chat">Chat</div>
+        <div className="chat">
+            <div className="top">
+                <div className="user">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <span>Bybis3</span>
+                        <p>Hello bybis</p>
+                    </div>
+                </div>
+                <div className="icons">
+                    <img src="./phone.png" alt="" />
+                    <img src="./video.png" alt="" />
+                    <img src="./info.png" alt="" />
+                </div>
+            </div>
+            <div className="center">
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="data">
+
+                    </div>
+                </div>
+                <div className="messageown">
+                    <img src="./info.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="data">
+
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="data">
+
+                    </div>
+                </div>
+                <div className="messageown">
+                    <img src="./info.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="data">
+
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <img src="https://images.steamusercontent.com/ugc/105104550292938732/3FE3F8BA895A13279F409C5C002EC73E2C254C5F/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false" alt="" />
+                        <p>
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="data">
+
+                    </div>
+                </div>
+            </div>
+            <div className="bottom">
+                <div className="icons">
+                    <img src="./img.png" alt=""/>
+                    <img src="./camera.png" alt=""/>
+                    <img src="./mic.png" alt=""/>
+                </div>
+                <input type="text" placeholder="Type.." value={text} onChange={e=>setText(e.target.value)}/>
+                <div className="emoji">
+                    <img src="./emoji.png" alt="" onClick={() => setOpen(prev => !prev)}/>
+                    <div className="picker">
+                        <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+                    </div>
+
+                </div>
+                <button className="sendButton">Send</button>
+            </div>
+        </div>
     )
     
 }
